07d7e942bb6ce8b2f9aa36f57785e0bbc296a03c
lyokocode <aelita.dev@gmail.com>
2023-12-07 04:31:24 +0300

    project restart

--- server/index.js
+++ server/index.js
@@ -3,13 +3,6 @@ import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
 
-// connection
-import { sequelize } from "./database/db.js"
-
-// route
-import userRoute from "./routes/user.js"
-
-
 const app = express()
 
 // middlewares
@@ -22,9 +15,7 @@ app.use("/", (req, res) => {
 
 async function main() {
     try {
-        await sequelize.sync(
-            { force: true }
-        );
+
         console.log("db connection is successfull")
         app.listen(process.env.PORT, () => console.log(`api is running on port: ${process.env.PORT}`))
 
