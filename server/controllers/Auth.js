3ced5ab14cfbb4485acec91731cb62c0a5885039
lyokocode <aelita.dev@gmail.com>
2023-12-08 05:55:30 +0300

    login & logout function

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -70,3 +70,37 @@ export const register = async (req, res, next) => {
         next(err);
     }
 }
+
+// LOGIN
+export const login = async (req, res, next) => {
+    try {
+        const user = await User.findOne({
+            where: {
+                userName: req.body.userName,
+            },
+        });
+
+        if (!user) return next(createError(404, "user not found"))
+
+        const isPsswordCorrect = await bcrypt.compare(req.body.password, user.password)
+        if (!isPsswordCorrect) return next(createError(400, "wrong password or username"))
+
+        const token = jwt.sign({
+            id: user.id,
+            isAdmin: user.isAdmin
+        }, process.env.JWT_SECRET_KEY, { expiresIn: '6h' })
+
+        const { password, ...info } = user.dataValues
+
+
+        return res
+            .cookie("access_token", token, {
+                httpOnly: true
+            })
+            .status(200)
+            .json({ ...info });
+
+    } catch (err) {
+        next(err)
+    }
+}
