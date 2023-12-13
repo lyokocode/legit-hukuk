ae0491cf116624c5afd3e631043f7e1d8132d82a
lyokocode <aelita.dev@gmail.com>
2023-12-13 06:23:13 +0300

    ScrollToTop

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,42 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import { Contact, Home, Team, Blogs, SingleBlog, SingleLawyer, NotFound, About, Services, SingleService } from "./pages"
 import Layout from "./utils/Layout"
+import { useLayoutEffect } from 'react'
+
 function App() {
+
+  const Wrapper = ({ children }) => {
+    const location = useLocation();
+    useLayoutEffect(() => {
+      document.documentElement.scrollTo(0, 0);
+    }, [location.pathname]);
+    return children
+  }
 
   return (
     <Router>
-      <Routes>
-        <Route element={<Layout />}>
+      <Wrapper>
+        <Routes>
+          <Route element={<Layout />}>
 
-          <Route path="/" element={<Home />} />
-          <Route path="/hakkimizda" element={<About />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/hakkimizda" element={<About />} />
 
-          <Route path="/hizmetlerimiz" element={<Services />} />
-          <Route path="/hizmetlerimiz/:slug" element={<SingleService />} />
+            <Route path="/hizmetlerimiz" element={<Services />} />
+            <Route path="/hizmetlerimiz/:slug" element={<SingleService />} />
 
-          <Route path="/makaleler" element={<Blogs />} />
-          <Route path="/makaleler/:slug" element={<SingleBlog />} />
+            <Route path="/makaleler" element={<Blogs />} />
+            <Route path="/makaleler/:slug" element={<SingleBlog />} />
 
-          <Route path="/ekibimiz" element={<Team />} />
-          <Route path="/ekibimiz/:slug" element={<SingleLawyer />} />
+            <Route path="/ekibimiz" element={<Team />} />
+            <Route path="/ekibimiz/:slug" element={<SingleLawyer />} />
 
-          <Route path="/iletisim" element={<Contact />} />
+            <Route path="/iletisim" element={<Contact />} />
 
-          <Route path='*' element={<NotFound />} />
-        </Route>
-      </Routes>
+            <Route path='*' element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Wrapper>
     </Router>
   )
 }
