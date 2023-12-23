b4632a1f607cd89095ce33be3b5c752bf26c8a84
lyokocode <aelita.dev@gmail.com>
2023-12-23 03:07:48 +0300

    pagination

--- client/src/components/BlogList/index.jsx
+++ client/src/components/BlogList/index.jsx
@@ -2,26 +2,24 @@ import "./blogList.scss";
 import { useSearchParams } from "react-router-dom";
 import useFetch from "@/hooks/useFetch";
 import { Pagination, SingleBlog } from "@/components";
-import { useState, useEffect } from "react";
 ""
 export const BlogList = () => {
+
     const [searchParams] = useSearchParams();
-    const [page, setPage] = useState(1);
-    const [pageSize, setPageSize] = useState(1);
+    let page = parseInt(searchParams.get('page')) || 1;
+    const pageSize = 1 // Sayfa başına blog sayısı
 
-    useEffect(() => {
-        // Sayfa parametresini al, yoksa varsayılan olarak 1'i kullan
-        const newPage = parseInt(searchParams.get("page")) || 1;
-        setPage(newPage);
-
-        // Sayfa büyüklüğü parametresini al, yoksa varsayılan olarak 1'i kullan
-        const newSize = parseInt(searchParams.get("pageSize")) || 1;
-        setPageSize(newSize);
-    }, [searchParams]);
 
     const { data: blogs, loading, error } = useFetch(
         `${import.meta.env.VITE_REACT_BASE_URL}/api/blogs?page=${page}&pageSize=${pageSize}`
     );
+
+
+    const handlePageChange = (newPage) => {
+        searchParams.set('page', newPage);
+        window.location.search = searchParams.toString();
+    };
+
 
     if (loading) {
         return "loading";
@@ -31,17 +29,21 @@ export const BlogList = () => {
         return "error";
     }
 
+
     return (
         <div className="test">
             <section className="blogList">
-                {blogs?.map((blog) => (
+                {blogs.length > 0 ? blogs?.map((blog) => (
                     <SingleBlog
                         key={blog.id}
                         blog={blog}
                     />
-                ))}
+                )) : "blog bulunamadı"}
 
-                <Pagination page={page} pageSize={pageSize} onPageChange={setPage} />
+                <Pagination
+                    currentPage={page}
+                    onPageChange={handlePageChange}
+                />
             </section>
         </div>
     );
--- client/src/components/Pagination/index.jsx
+++ client/src/components/Pagination/index.jsx
@@ -1,17 +1,23 @@
 import "./pagination.scss";
 import { GrPrevious, GrNext } from "react-icons/gr";
 
-export const Pagination = () => {
+export const Pagination = ({ currentPage, totalPages, onPageChange }) => {
 
 
     return (
         <div className="pagination">
             <button className="button prev"
+                onClick={() => onPageChange(currentPage - 1)}
+                disabled={currentPage === 1}
+                style={{ backgroundColor: currentPage === 1 ? "gray" : "" }}
             >
                 <GrPrevious />
                 Önceki
             </button>
+
             <button className="button next"
+                onClick={() => onPageChange(currentPage + 1)}
+                disabled={currentPage === totalPages}
             >
                 Sonraki
                 <GrNext />
